93448c302e64c6faabc1420340b611429b8d2a1b
yjk0401 <136331694+yjk0401@users.noreply.github.com>
2023-06-12 21:53:53 +0900

    firstproject
    commited firstproject

---
+++ csharpbasic/first project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace first_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
