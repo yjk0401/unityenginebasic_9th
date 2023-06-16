a238fdf1df44ae13e36012e2d05040ecf6f02e8c
yjk0401 <136331694+yjk0401@users.noreply.github.com>
2023-06-16 21:52:11 +0900

    day5
    필기
    연산 및 정리

---
+++ csharpbasic/Statement_if/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Statement_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool conditional1 = true;
+            bool conditional2 = false;
+
+
+            if (conditional1)
+            {
+                Console.WriteLine("조건 1이 참");
+            }
+            else if (conditional2) 
+            {
+                Console.WriteLine("위 조건들이 모두 거짓이면서 조건 2가 참");
+            }
+            else 
+            {
+                Console.WriteLine("위 조건들이 모두 거짓");            
+            }
+        }
+    }
+}
