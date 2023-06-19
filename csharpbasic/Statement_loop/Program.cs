d3723ccc750d063c98020ca76968c3d2df39f0b4
yjk0401 <136331694+yjk0401@users.noreply.github.com>
2023-06-19 21:40:53 +0900

    week2 day6
    Array

---
+++ csharpbasic/Statement_loop/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Statement_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // while 문
+            // 특정조건에서 특정 내용을 반복 수행
+            int count = 0;
+            while (count < 5)
+            {
+                Console.WriteLine("Repeating!!");
+                count++;
+            }
+
+            // do while 문
+            // 일단 한번 반복내용 수행하고, 그 위
+            // 특정조건에서 특정내용을 반복수행
+            do
+            {
+                Console.WriteLine("do while");
+            } while (false);
+
+            // for 문
+            //for (처음 한번 실행할 내용; 반복 내용; 루프의 마지막에 실행할 내용)
+            //{
+            //
+            //}
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("For looping!!");
+            }
+        }
+    }
+}
