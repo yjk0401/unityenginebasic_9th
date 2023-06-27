3bfcbfbb3b690ad8e9fad9b5359f2b0d1ec5a8cb
yjk0401 <136331694+yjk0401@users.noreply.github.com>
2023-06-27 21:52:09 +0900

    week3 day12

--- csharpbasic/Inheritance/Program.cs
+++ csharpbasic/Inheritance/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 namespace Inheritance
 {
@@ -9,9 +10,9 @@ namespace Inheritance
             Knight knight = new Knight();
             knight.hp = 10;
             HpBar knightHpBar = new HpBar();
+            IDamageable damageable = knight;
 
-            knight.onHpChanged += knightHpBar.Refresh;
-            knight.onHpChanged += knightHpBar.Refresh;
+            knight.onHpChanged += knightHpBar.Refresh; 
             knight.onHpChanged += knightHpBar.Refresh;
             knight.onHpChanged += knightHpBar.Refresh;
             //knight.onHpChanged = knightHpBar.Refresh;
@@ -21,7 +22,6 @@ namespace Inheritance
             };
             // 람다식 표현
             // 컴파일러가 알아서 유추할 수 있는 내용을 모두 지운 뒤, => 이 기호로 람다식이라는 명사만 해주면 됌.
-
 
             while (true)
             {
@@ -47,14 +47,14 @@ namespace Inheritance
             ((NPC)creature2).Interation();
 
             // is : 왼쪽 객체가 오른쪽 타입으로 캐스팅이 가능하다면 true, 아니면 false 를 반환하는 키워드
-            if (creature2 is NPC) 
+            if (creature2 is NPC)
             {
                 ((NPC)creature2).Interation();
             }
 
             // as : 왼쪽 객체를 오른쪽 타입으로 캐스팅 시도하고 성공시 캐스팅된 타입참조반환, 실패시 null 반환
             NPC tmpNPc = creature2 as NPC;
-            if (tmpNPc != null) 
+            if (tmpNPc != null)
             {
                 tmpNPc.Interation();
             }
@@ -68,17 +68,75 @@ namespace Inheritance
             {
                 characters[i].UniqueSkill();
 
-                if (characters[i] is Knight) 
+                if (characters[i] is Knight)
                     Console.WriteLine("기사 발견!");
 
-                if (characters[i] is Magician) 
+                if (characters[i] is Magician)
                     Console.WriteLine("마법사 발견!");
 
-                if (characters[i] is SwordMan) 
+                if (characters[i] is SwordMan)
                     Console.WriteLine("전사 발견!");
             }
+
+
+        }
+
+        public void Test() 
+        {
+            PrintSomething(3);
+            PrintSomething<double>(3); 
+            PrintSomething<long>(3);
+            // Generic 타입 자체가 코드영역에 할당되는게 아니고
+            // (런타임중에 제네릭 참조해서 새로운 함수를 만들어서 스택영역에 쌓는게 아님)
+            // 컴파일  타임에 컴파일러가 사용되고 있는 형태를 참수로 새로 만들어서 코드영역에 할당함
+            // ex) Something<T>() 함수가 있고, Something<int>() 라는 함수호출을 어딘가에서 하고있으면
+            // 컴파일타임에 Something<int>() 를 만든다.
+
+            Dummy<int> dummy1 = new Dummy<int>();
+            Dummy<float> dummy2 = new Dummy<float>();
+            Dummy<Knight> dummy3 = new Dummy<Knight>();
+            Dummy<IDamageable> dummy4 = new Dummy<IDamageable>();
         }
 
 
+        public void PrintSomething(int value) 
+        {
+            Console.WriteLine(value);
+        }
+
+        public void PrintSomething(float value) 
+        {
+            Console.WriteLine(value);
+        }  
+
+        public void PrintSomething(string value) 
+        {
+            Console.WriteLine(value);
+        }
+
+        public void PrintSomething<T>(T value) 
+        {
+            Console.WriteLine(value);
+        }
+
     }
+
+    // 제네릭 클래스
+    public class Dummy<T> 
+    {
+        public T value;
+
+        public void SaySomething() 
+        {
+            T value1 = value;
+            What<T>();
+            
+        }
+
+        public void What<T>() 
+        {
+        
+        }
+    }
+
 }
