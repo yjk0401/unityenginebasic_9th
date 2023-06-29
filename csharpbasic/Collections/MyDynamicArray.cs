710e89f9b41ca81bbd63c503de37428620556101
yjk0401 <136331694+yjk0401@users.noreply.github.com>
2023-06-29 21:57:52 +0900

    week3 day 14

---
+++ csharpbasic/Collections/MyDynamicArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class MyDynamicArray
+    {
+        public int Length => _Length;
+        public int Capacity => _items.Length;
+        private object[] _items;
+        private int _Length;
+
+        public void Add(object item) 
+        {
+            // 공간이 모자라다면 더 큰배열을 만들고 아이템 복제
+            if (_Length == _items.Length) 
+            {
+                object[] tmpItems = new object[_Length * 2];
+                Array.Copy(_items, 0, tmpItems, 0, _Length);
+                _items = tmpItems;
+            }
+
+            _items[_Length++] = item;
+        }
+        // <summary>
+        // 탐색 알고리즘
+        // </summary>
+        // <parer name = "item"></parer>
+        // <returns> 삭제 여부 </returns>
+        public int IndexOf(object item) 
+        {
+            Comparer comparer = Comparer.Default;
+            // Comparer.Default : 해당 타입의 default 비교 연산자를 가지고 비교해서 결과를 반환하는 기능을 가진 객체
+            for (int i = 0; i < _Length; i++)
+            {
+                if (comparer.Compare(_items[i], item) == 0)
+                    return i;
+            }
+            return -1;
+        }
+        // <summary>
+        // 삭제 알고리즘
+        // </summary>
+        // <returns> 삭제 여부 </returns>
+        public bool Remove(object item)
+        {
+            int index = IndexOf(item);
+            if (index < 0)
+                return false;
+
+            for (int i = index; i < _Length - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _Length--;
+            return true;
+        }
+    }
+}
---
+++ csharpbasic/Collections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue1 = new Queue<int>();
+            queue1.Enqueue(3);
+            queue1.Dequeue();
+
+            while (queue1.Count > 0)
+            {
+                if (queue1.Peek() > 1)
+                    Console.WriteLine(queue1.Dequeue());
+            }
+
+            Stack<float> stack1 = new Stack<float>();
+            stack1.Push(3);
+            stack1.Push(2);
+            stack1.Push(5);
+
+            while (stack1.Count > 0)
+            {
+                if (stack1.Peek() > 1)
+                    Console.WriteLine(stack1.Pop());
+            }
+
+            HashSet<int> visited = new HashSet<int>();
+            if (visited.Add(3))
+            {
+                Console.WriteLine("Added 3 in hashset");
+            }
+            if (visited.Remove(2))
+            {
+                Console.WriteLine("Remove 2 in hashset");
+            }
+
+            
+        }
+    }
+}
