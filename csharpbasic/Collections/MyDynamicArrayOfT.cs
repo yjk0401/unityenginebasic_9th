455c04b60977efb8de02790b10e544c70fc04ac3
yjk0401 <136331694+yjk0401@users.noreply.github.com>
2023-07-03 21:52:15 +0900

    week4 day16

--- csharpbasic/Collections/MyDynamicArrayOfT.cs
+++ csharpbasic/Collections/MyDynamicArrayOfT.cs
@@ -8,76 +8,109 @@ using System.Threading.Tasks;
 namespace Collections
 {
     internal class MyDynamicArray<T> : IEnumerable<T>
+        where T : IComparable<T>
     {
         private static int DEFAULT_SIZE = 1;
-        public int Length => _Length;
+        public int Count => _Count;
         public int Capacity => _items.Length;
-        private object[] _items = new object[DEFAULT_SIZE];
-        private int _Length;
+        private T[] _items = new T[DEFAULT_SIZE];
+        private int _Count;
 
         /// <summary>
         ///  삽입 알고리즘
         /// </summary>
         /// <param name="item"></param>
 
-        public void Add(object item) 
+        public void Add(T item) 
         {
             // 공간이 모자라다면 더 큰배열을 만들고 아이템 복제
-            if (_Length == _items.Length) 
+            if (_Count == _items.Length) 
             {
-                object[]tmpItems = new object[_Length * 2];
-                Array.Copy(_items, 0, tmpItems, 0, _Length);
+                T[]tmpItems = new T[_Count * 2];
+                Array.Copy(_items, 0, tmpItems, 0, _Count);
                 _items = tmpItems;
             }
 
-            _items[_Length++] = item;
+            _items[_Count++] = item;
         }
+
         /// <summary>
         /// 탑색 알고리즘
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
-        public int IndexOf(object item) 
+        public int IndexOf(T item) 
         {
-            Comparer comparer = Comparer.Default;
-            // Comparer.Default : 해당 타입의 default 비교 연산자를 가지고 비교해서 결과를 반환하는 기능을 가진 객체
-            for (int i = 0; i < _Length; i++)
+            for (int i = 0; i < _Count; i++)
             {
-                if (comparer.Compare(_items[i], item) == 0)
+                if (Comparer<T>.Default.Compare(_items[i], item) == 0)
                     return i;
             }
             return -1;
         }
+
         /// <summary>
         /// 삭제 알고리즘
         /// </summary>
         /// <param name="item"></param>
         /// <returns></returns>
-        public bool Remove(object item)
+        public bool Remove(T item)
         {
             int index = IndexOf(item);
             if (index < 0)
                 return false;
 
-            for (int i = index; i < _Length - 1; i++)
+            for (int i = index; i < _Count - 1; i++)
             {
                 _items[i] = _items[i + 1];
             }
-            _Length--;
+            _Count--;
             return true;
         }
 
-        public IEnumerator GetEnumerator()
+        IEnumerator IEnumerable.GetEnumerator()
         {
-           for (int i = 0; i < _Length; i++)
+           for (int i = 0; i < _Count; i++)
            {
                yield return _items[i];
            }
         }
 
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new Enumerator(this);
+        }
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            public T Current => _data._items[_index];
+
+            object IEnumerator.Current => _data._items[_index];
+
+            private MyDynamicArray<T> _data;
+            private int _index;
+
+            public Enumerator(MyDynamicArray<T> data) 
+            {
+                _data = data;
+                _index = -1;
+            }
+
+            public void Dispose()
+            {
+                
+            }
+
+            public bool MoveNext()
+            {
+                _index++;
+                return _index < _data._Count;
+            }
+
+            public void Reset()
+            {
+                _index = -1;
+            }
         }
     }
 }
--- csharpbasic/Collections/Program.cs
+++ csharpbasic/Collections/Program.cs
@@ -7,6 +7,7 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            #region Queue
             Queue<int> queue1 = new Queue<int>();
             queue1.Enqueue(3);
             queue1.Dequeue();
@@ -21,13 +22,17 @@ namespace Collections
             stack1.Push(3);
             stack1.Push(2);
             stack1.Push(5);
+            #endregion
 
+            #region Stack
             while (stack1.Count > 0)
             {
                 if (stack1.Peek() > 1)
                     Console.WriteLine(stack1.Pop());
             }
+            #endregion
 
+            #region HashSet
             HashSet<int> visited = new HashSet<int>();
             if (visited.Add(3))
             {
@@ -37,6 +42,7 @@ namespace Collections
             {
                 Console.WriteLine("Remove 2 in hashset");
             }
+            
 
             MyDynamicArray myDynamicArray = new MyDynamicArray();
             myDynamicArray.Add(3);
@@ -49,8 +55,46 @@ namespace Collections
             {
                 Console.WriteLine(e1.Current);
             }
-            e1.Reset();
 
+            MyDynamicArray<int> intDA = new MyDynamicArray<int>();
+            intDA.Add(3);
+            intDA.Add(2);
+            intDA.Add(5);
+            intDA.Add(7);
+
+            IEnumerator<int> intDAEnum = intDA.GetEnumerator();
+            while (intDAEnum.MoveNext())
+            {
+                Console.WriteLine(intDAEnum.Current);
+            }
+            intDAEnum.Reset();
+            intDAEnum.Dispose();
+
+            // IDisposable 객체의 Dispose() 함수의 호출을 보장하는 구문.
+            using (IEnumerator<int> intDAEnum2 = intDA.GetEnumerator()) 
+            {
+                while (intDAEnum.MoveNext())
+                {
+                    Console.WriteLine(intDAEnum.Current);
+                }
+                intDAEnum.Reset();
+            }
+
+            // foreach 문
+            // IEnumerable 을 순회하는 구문
+            foreach (int item in intDA)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<int> List = new List<int>();
+            List.Add(3);
+            List.Remove(3);
+            List.IndexOf(3);
+            List.Contains(3);
+            List.Find(x => x > 1);
+            List.Insert(0, 2);
+            #endregion
         }
     }
 }
