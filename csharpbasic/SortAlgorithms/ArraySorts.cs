0a0672ed1968cb448bc0f99c476265c418160db7
yjk0401 <136331694+yjk0401@users.noreply.github.com>
2023-07-20 21:50:41 +0900

    r2w2d7

--- csharpbasic/SortAlgorithms/ArraySorts.cs
+++ csharpbasic/SortAlgorithms/ArraySorts.cs
@@ -20,22 +20,74 @@ namespace SortAlgorithms
             int i, j;
             for (i = 0; i < arr.Length - 1; i++)
             {
-                for (j = 0; j < arr.Length - (1 + i) ; j++)
+                for (j = 0; j < arr.Length - 1 - i ; j++)
                 {
                     if (arr[j] > arr[j + 1]) 
                     {
-                        Swap(ref arr[j], arr[j + 1]);
+                        Swap(ref arr[j], ref arr[j + 1]);
                     }
                 }
             }
         }
 
-        // ref : 인자를 변수의 참조로 받아야 할 때 사용하는 키워드
-        public static void Swap(ref int a, int b) 
+        /// <summary>
+        /// 선택 정렬
+        /// O(N^2)
+        /// Unstable
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void SelectionSort(int[] arr) 
         {
-            int tmp = a;
+            int i, j, minIdx;
+            for (i = 0; i < arr.Length; i++)
+            {
+                minIdx = i;
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIdx])
+                        minIdx = j;
+                }
+
+                Swap(ref arr[i], ref arr[minIdx]);
+            }
+        }
+
+        /// <summary>
+        /// 삽입정렬
+        /// O(N^2)
+        /// stable
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void InsertionSort(int[] arr) 
+        {
+            int i, j;
+            int key;
+
+            for (i = 1; i < arr.Length; i++)
+            {
+                key = arr[i];
+                j= i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
+        public static void MergeSort(int[] arr) 
+        {
+            
+        }
+
+
+        // ref : 인자를 변수의 참조로 받아야 할 때 사용하는 키워드
+        public static void Swap(ref int a, ref int b) 
+        {
+            int tmp = b;
             b = a;
-            b = tmp;
+            a = tmp;
         }
 
     }
--- csharpbasic/SortAlgorithms/Program.cs
+++ csharpbasic/SortAlgorithms/Program.cs
@@ -1,18 +1,36 @@
-﻿namespace SortAlgorithms
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace SortAlgorithms
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 4, 3, 3, 9, 8, 7, 2, 5, 0 };
 
-            ArraySorts.BubbleSort(arr);
+            Random random = new Random();
+            int[] arr //= { 1, 4, 3, 3, 9, 8, 7, 2, 5, 0 };
+                        = Enumerable
+                            .Repeat (0, 10000)
+                            .Select (i => random.Next(0, 10000))
+                            .ToArray ();
 
-            Console.Write("Result : ");
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write($"{arr[i]}, ");
-            }
+            Stopwatch stopwatch = new Stopwatch ();
+            stopwatch.Start ();
+
+            //ArraySorts.BubbleSort(arr);
+            //ArraySorts.SelectionSort(arr);
+            ArraySorts.InsertionSort (arr);
+
+            stopwatch.Stop ();
+            Console.WriteLine($"소요시간 : {stopwatch.ElapsedMilliseconds}");
+
+            //Console.Write("Result : ");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"{arr[i]}, ");
+            //}
         }
     }
 }
