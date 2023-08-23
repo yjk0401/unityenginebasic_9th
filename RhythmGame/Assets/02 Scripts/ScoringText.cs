8c3904f0b219b76daa9cb8a2e55bb6a768763ff8
yjk0401 <136331694+yjk0401@users.noreply.github.com>
2023-08-23 21:55:01 +0900

    r3w2d10

---
+++ RhythmGame/Assets/02 Scripts/ScoringText.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Text;
+
+public class ScoringText : MonoBehaviour
+{
+    public int score 
+    {
+        get => _score;
+        set
+        {
+            _after = value;
+            _score = value;
+            _delta = (int)((_after - _before) / _duration);
+        }
+    }
+
+    private int _score;
+    private int _before;
+    private int _after;
+    private float _delta;
+    private float _duration = 0.1f;
+    private TMP_Text _scoreText;
+    private StringBuilder _stringBuilder;
+    private string format = "{0,3:" + "000" + "}";
+
+    private void Awake()
+    {
+        _scoreText = GetComponent<TMP_Text>();
+        _scoreText.text = score.ToString();
+        _stringBuilder = new StringBuilder();
+    }
+
+    private void Update()
+    {
+        if (_before < _after) 
+        {
+            _before += (int)(_delta * Time.deltaTime);
+
+            if (_before > _after)
+                _before = _after;
+
+            _stringBuilder.Clear();
+            _stringBuilder.AppendFormat(("{0:N0}"), _before);
+            _scoreText.text = _stringBuilder.ToString();
+        }
+    }
+}
